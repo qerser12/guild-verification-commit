47a3b3333fd451887038537195a36723bce8b461
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:17:04 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:16:44.702Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T00:17:03.549Z
+Commit: Resolve null pointer exception in user service
