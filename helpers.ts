5bb272e18c7b901a767418c292c3f21bc3112c8b
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:21:13 +0300

    Fix database connection timeout

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:21:06.951Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T00:21:11.819Z
+Commit: Fix database connection timeout
