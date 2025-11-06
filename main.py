584f00a56112d842a57a4f6f43d3b3d706d44f07
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:21:48 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:21:38.486Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T00:21:47.519Z
+Commit: Fix CSS styling bug on mobile devices
