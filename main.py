675de1866c0e613891c9adcbf95ab591d24358ae
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:17:56 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:17:53.571Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T00:17:54.909Z
+Commit: Fix broken links in documentation
