ed300a3405046fac882c9e4058891e817f5ce77e
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:21:57 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:21:51.817Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T00:21:55.931Z
+Commit: Fix memory leak in event handlers
