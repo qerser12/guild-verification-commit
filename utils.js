d351ac9787bbb5f7e1d9e76b9ef847297e1eb6ee
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:11:59 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:11:45.977Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T00:11:57.500Z
+Commit: Fix memory leak in event handlers
