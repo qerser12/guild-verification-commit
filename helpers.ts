c07236d63f5c792d2abc1904da498694584aeaad
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:12:00 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:11:55.849Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T00:11:59.062Z
+Commit: Resolve timezone issues in date handling
