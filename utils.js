993d0753c5abff2402f58b14217d3f99132a2c0f
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:12:32 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:11:57.500Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T00:12:30.868Z
+Commit: Fix CSS styling bug on mobile devices
