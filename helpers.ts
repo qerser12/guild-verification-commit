0ccbe1031378fdf7bfa8d66f4e7288d98606eede
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:11:57 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:11:15.140Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T00:11:55.849Z
+Commit: Fix CSS styling bug on mobile devices
