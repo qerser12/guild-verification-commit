8d3a573077bdaa62b508c89fc55bc96fe16c4103
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:20:59 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:17:54.909Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T00:20:58.165Z
+Commit: Patch API endpoint error handling
