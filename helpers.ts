6bb6986ab6ee737ddc73c4156271ef9eb40de00c
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:12:12 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:11:59.062Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T00:12:10.700Z
+Commit: Add multi-language support
