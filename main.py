7c49b7e4c600fa11ab1bbc02958d7972148c70ad
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:21:23 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:21:21.071Z
-Commit: Add multi-language support
+Timestamp: 2025-11-06T00:21:22.345Z
+Commit: Resolve timezone issues in date handling
