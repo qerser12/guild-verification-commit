d333ad00057696055f2986670503dc1d78a881de
toreto <113203832+qerser12@users.noreply.github.com>
2025-11-06 03:10:52 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:10:49.566Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T00:10:51.189Z
+Commit: Resolve null pointer exception in user service
